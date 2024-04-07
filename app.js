6c4e886d41052377bd15bb484906336a9746e517
Jaeseok Shin <142656933+jaeseokshin5245@users.noreply.github.com>
2024-04-07 13:38:08 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -9,14 +9,12 @@ app.set("port", port);
 app.get(`/`, (req, res) => {
   console.log(req.query);
   res.status(200);
-  res.send(req.query);
   res.send({"result": "GET 호출"});
 })
 
 app.post(`/post`, (req, res) => {
   console.log(req.body);
   res.status(200);
-  res.send(req.body)
   res.send({"result": "POST 호출"});
 })
 
